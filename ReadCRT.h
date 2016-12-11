aec7c1a2483bbec327731e3ec86bd20cbd4554ae
Patrick Kenekayoro <ktarila@gmail.com>
2016-12-11 16:18:10 +0100

    Fixed simple bugs to compile

--- ReadCRT.h
+++ ReadCRT.h
@@ -8,66 +8,61 @@
 #ifndef READCRT_H_
 #define READCRT_H_
 
-#include "Schedule/courseroomtime.h"
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <iterator>
 #include <sstream>
 #include <vector>
-#include <algorithm>
+#include "Schedule/courseroomtime.h"
 
-vector<CourseRoomTime> readCRT(vector<Course> courses, vector<Venue> rooms, int periods_per_day, const char* filename)
-{
+vector<CourseRoomTime> readCRT(vector<Course> courses, vector<Venue> rooms,
+                               int periods_per_day, const char *filename) {
+  vector<CourseRoomTime> jCRT;
+  string line;
+  std::ifstream myfile(filename);
+  if (myfile.is_open()) {
+    while (getline(myfile, line)) {
+      istringstream ss(line);
+      istream_iterator<string> begin(ss), end;
 
-	vector<CourseRoomTime> jCRT;
-	string line;
-	ifstream myfile (filename);
- 	if (myfile.is_open())
-	{
-		while ( getline (myfile,line) )
-		{
-			istringstream ss(line);
-			istream_iterator<string> begin(ss), end;
+      // putting all the tokens in the vector
+      vector<std::string> arrayTokens(begin, end);
+      // string* lineArray = &arrayTokens[0];  //pointer to vector so can access
+      // as array
 
-			//putting all the tokens in the vector
-			vector<std::string> arrayTokens(begin, end);
-			//string* lineArray = &arrayTokens[0];  //pointer to vector so can access as array
+      if (&arrayTokens[0] != NULL) {
+        int period = 0;
+        int day = 0;
+        string cCode, rCode;
+        string *token0 = &arrayTokens[0];
+        string *token1 = &arrayTokens[1];
+        string *token2 = &arrayTokens[2];
+        string *token3 = &arrayTokens[3];
 
-			if (&arrayTokens[0] != NULL)
-			{
-				int period = 0;
-				int day = 0;
-				string cCode, rCode;
-				string *token0 = &arrayTokens[0];
-				string *token1 = &arrayTokens[1];
-				string *token2 = &arrayTokens[2];
-				string *token3 = &arrayTokens[3];
+        istringstream(*token2) >> day;
+        istringstream(*token3) >> period;
+        rCode = *token1;
+        Course temp(*token0);
+        std::vector<Course>::iterator findCourse =
+            std::find(courses.begin(), courses.end(), temp);
 
-				istringstream(*token2) >> day;
-				istringstream(*token3) >> period;
-				rCode = *token1;
-				Course temp(*token0);
-				std::vector<Course>::iterator findCourse = std::find(courses.begin(), courses.end(), temp);
-
-				Venue temp1(*token1);
-				std::vector<Venue>::iterator findRoom = std::find(rooms.begin(), rooms.end(), temp1);
-				if (findCourse != courses.end() && findRoom != rooms.end())
-				{
-					int periodValue = (day * periods_per_day) + period;
-					Course c = *findCourse;
-					Venue r = *findRoom;
-					CourseRoomTime crt(c, RoomTime(periodValue, r));
-					jCRT.push_back(crt);
-				}
-			}
-		}
-		myfile.close();
-	}
-	else std::cout << "Unable to open file";
- 	return jCRT;
-
-
+        Venue temp1(*token1);
+        std::vector<Venue>::iterator findRoom =
+            std::find(rooms.begin(), rooms.end(), temp1);
+        if (findCourse != courses.end() && findRoom != rooms.end()) {
+          int periodValue = (day * periods_per_day) + period;
+          Course c = *findCourse;
+          Venue r = *findRoom;
+          CourseRoomTime crt(c, RoomTime(periodValue, r));
+          jCRT.push_back(crt);
+        }
+      }
+    }
+    myfile.close();
+  } else
+    std::cout << "Unable to open file";
+  return jCRT;
 }
-
-
 
 #endif /* READCRT_H_ */
--- Timetable.cpp
+++ Timetable.cpp
@@ -1,143 +1,162 @@
 // reading a text file
-#include "data.h"
-#include "ReadCRT.h"
-#include "Schedule/smethods.h"
-#include <string>
 #include <sstream>
+#include <string>
+#include "ReadCRT.h"
+#include "Schedule/enhancement.h"
 #include "Schedule/feasibletable.h"
 #include "Schedule/improvetable.h"
-#include "Schedule/enhancement.h"
-
+#include "Schedule/smethods.h"
+#include "data.h"
 
 using namespace std;
 
-int main () {
-	 srand(time(NULL));
-	const char* filename = "InputData/ITC-2007_ectt/comp11.ectt";
-	const char* solfilename = "/home/patrick/work/UCTP-CPP/Results/comp11.sol";
-	//const char* filename = "InputData/Test_ectt/toy.ectt";
-	Data* data = new Data(filename);
+int main() {
+  srand(time(NULL));
+  const char* filename = "InputData/ITC-2007_ectt/comp11.ectt";
+  const char* solfilename = "/home/patrick/work/UCTP-CPP/Results/comp11.sol";
+  // const char* filename = "InputData/Test_ectt/toy.ectt";
+  Data* data = new Data(filename);
 
+  cout << "\nName: " << data->getName() << endl;
+  cout << "Courses: " << data->getNumCourses() << endl;
+  cout << "Rooms: " << data->getNumRooms() << endl;
+  cout << "Days: " << data->getNumDays() << endl;
+  cout << "Periods_per_day: " << data->getNumPeriodsPerDay() << endl;
+  cout << "Curricula: " << data->getNumCurricula() << endl;
+  cout << "Min_Max_Daily_Lectures: " << data->getMinDaily() << " "
+       << data->getMaxDaily() << endl;
+  cout << "UnavailabilityConstraints: "
+       << data->getNumUnavailabilityConstraint() << endl;
+  cout << "RoomConstraint (Feature): " << data->getNumRoomConstraints() << endl;
 
-	cout<<"\nName: " << data->getName()<<endl;
-	cout<<"Courses: " << data->getNumCourses()<<endl;
-	cout<<"Rooms: " << data->getNumRooms()<<endl;
-	cout<<"Days: " << data->getNumDays()<<endl;
-	cout<<"Periods_per_day: " << data->getNumPeriodsPerDay()<<endl;
-	cout<<"Curricula: " << data->getNumCurricula()<<endl;
-	cout<<"Min_Max_Daily_Lectures: " << data->getMinDaily() << " " <<data->getMaxDaily()<<endl;
-	cout<<"UnavailabilityConstraints: " << data->getNumUnavailabilityConstraint()<<endl;
-	cout<<"RoomConstraint (Feature): " << data->getNumRoomConstraints()<<endl;
+  int maximumPeriod = (data->getNumDays() * data->getNumPeriodsPerDay()) - 1;
+  SM::numPeriods = maximumPeriod + 1;
 
-	int maximumPeriod = (data->getNumDays() * data->getNumPeriodsPerDay()) - 1;
-	SM::numPeriods = maximumPeriod + 1;
+  FeasibleTable ft(data->getCourses(), data->getRooms(), maximumPeriod,
+                   data->getNumPeriodsPerDay(), data->getCurriculum());
+  // std::clock_t t1,t2;
+  // ft.printAdjMat();
 
-	FeasibleTable ft(data->getCourses(),  data->getRooms(), maximumPeriod,  data->getNumPeriodsPerDay(), data->getCurriculum());
-	//std::clock_t t1,t2;
-	//ft.printAdjMat();
+  cout << endl << endl;
 
+  cout << "Construction ...." << endl;
+  double wall0 = SM::get_wall_time();
+  double cpu0 = SM::get_cpu_time();
+  ft.antColonyThread(2, 100);
 
-	cout<<endl<<endl;
+  //auto fromOld = readCRT(data->getCourses(), data->getRooms(),
+  //                       data->getNumPeriodsPerDay(), solfilename);
+  // ft.setFeasibleTable(fromOld);
+  cout << " Timetable has " << ft.NumberHCV()
+       << " number of hard constraint violations" << endl;
 
+  cout << endl << endl << "Improvement Phase..." << endl;
 
+  ImproveTable impTable(ft.getVenueTime(), data->getRooms(), ft.getCurCodes(),
+                        ft.getCourse(), ft.getFeasibleTable(),
+                        ft.getMaxPeriod(), ft.getPeriodsInDay());
+  // int max = ft.getFeasibleTable().size() - 1;
+  // int min = 0;
+  // int crtOneIndex = min + (rand() % (int)(max - min + 1));  //generate random
+  // number in range [min max]
+  // int crtTwoIndex = min + (rand() % (int)(max - min + 1));  //generate random
+  // number in range [min max]
+  // cout<<"Index One: "<<crtOneIndex<<" Index Two: "<<crtTwoIndex<<endl;
+  // auto timet = impTable.singleMove(10, 180, ft.getFeasibleTable());
+  /*auto timet= fromOld;
+  for (int j =0; j < 10; j++)
+  {
+      for (size_t i =0; i < ft.getFeasibleTable().size(); i++)
+      {
+              cout<<"Iteration "<<i<<": ";
+              timet = impTable.bestNeighbour((int)i, ft.getFeasibleTable());
+              auto currentSCV = impTable.NumberSCV(timet);
+              std::cout<< currentSCV<<" is the current SCV"<<endl;
+              if (currentSCV < impTable.NumberSCV(fromOld))
+              {
+                      //cout<<" I am here "<<impTable.NumberSCV(fromOld)<<endl;
+                      fromOld = timet;
+                      //timet = fromOld;
+              }
+      }
+      ft.setFeasibleTable(fromOld);
+      cout<<"number of violations in iteration "<<j << " is "<<
+  impTable.NumberSCV(ft.getFeasibleTable());
+  }*/
 
-	cout<<"Construction ...."<<endl;
-    double wall0 =  SM::get_wall_time();
-    double cpu0  = SM::get_cpu_time();
-	ft.antColonyThread(1, 1);
+  // vector<int> chromosome;
+  // auto timet = impTable.applyChomosome(chromosome, ft.getFeasibleTable());
+  // auto timet = impTable.bestNeighbour(10, ft.getFeasibleTable());
+  // auto newT = timet;
+  auto newT = ft.getFeasibleTable();
+  std::stringstream ss;
+  ss << "Solutions/" << data->getName();
+  impTable.writeTimetableToFileSolutionFormat(ss.str(), newT);
 
+  double wall6 = SM::get_wall_time();
+  double cpu6 = SM::get_cpu_time();
+  std::cout << endl
+            << "Full Timetable construction " << wall6 - wall0
+            << " wall seconds and " << cpu6 - cpu0 << " CPU seconds" << endl;
 
-	auto fromOld = readCRT(data->getCourses(), data->getRooms(), data->getNumPeriodsPerDay(), solfilename);
-  	ft.setFeasibleTable(fromOld);
-  	cout<<" Timetable has "<<ft.NumberHCV()<<" number of hard constraint violations"<<endl;
+  int roomStabilityViolations = 0;
+  int courseWorkingDayViolations = 0;
+  int consecutiveLectureViolations = 0;
+  int sizeViolations = 0;
 
+  cout << endl
+       << endl
+       << "***********************Improved Timetable2*****************" << endl;
+  cout << endl
+       << endl
+       << " New Timetable has " << ft.NumberHCV(newT)
+       << " number of hard constraint violations" << endl;
+  cout << " New Timetable has "
+       << impTable.NumberSCV(newT, &roomStabilityViolations,
+                             &courseWorkingDayViolations,
+                             &consecutiveLectureViolations, &sizeViolations)
+       << " number of soft constraint violations" << endl;
+  cout << "Room Size Violations: " << sizeViolations
+       << " Room Stability: " << roomStabilityViolations
+       << " Course Working Day Violations: " << courseWorkingDayViolations
+       << " Consecutive Lecture Violations: " << consecutiveLectureViolations
+       << endl;
 
-	cout<<endl<<endl<<"Improvement Phase..."<<endl;
+  cout << endl << "**************Old Timetable**********************" << endl;
+  cout << " Timetable has " << ft.NumberHCV()
+       << " number of hard constraint violations" << endl;
+  cout << " Old Timetable has "
+       << impTable.NumberSCV(ft.getFeasibleTable(), &roomStabilityViolations,
+                             &courseWorkingDayViolations,
+                             &consecutiveLectureViolations, &sizeViolations)
+       << " number of soft constraint violations" << endl;
+  cout << "Room Size Violations: " << sizeViolations
+       << " Room Stability: " << roomStabilityViolations
+       << " Course Working Day Violations: " << courseWorkingDayViolations
+       << " Consecutive Lecture Violations: " << consecutiveLectureViolations
+       << endl;
 
+  cout << "Threaded HCV: " << SM::ThreadNumberHCV(newT, (maximumPeriod + 1));
+  impTable.writeTimetableToFile("ImproveTab.csv");
 
-    ImproveTable impTable(ft.getVenueTime(), data->getRooms(), ft.getCurCodes(), ft.getCourse(), ft.getFeasibleTable(), ft.getMaxPeriod(), ft.getPeriodsInDay());
-    //int max = ft.getFeasibleTable().size() - 1;
-    //int min = 0;
-    //int crtOneIndex = min + (rand() % (int)(max - min + 1));  //generate random number in range [min max]
-    //int crtTwoIndex = min + (rand() % (int)(max - min + 1));  //generate random number in range [min max]
-    //cout<<"Index One: "<<crtOneIndex<<" Index Two: "<<crtTwoIndex<<endl;
-    //auto timet = impTable.singleMove(10, 180, ft.getFeasibleTable());
-    auto timet= fromOld;
-    for (int j =0; j < 10; j++)
-    {
-    	for (size_t i =0; i < ft.getFeasibleTable().size(); i++)
-    	{
-    		cout<<"Iteration "<<i<<": ";
-    		timet = impTable.bestNeighbour((int)i, ft.getFeasibleTable());
-    		auto currentSCV = impTable.NumberSCV(timet);
-    		std::cout<< currentSCV<<" is the current SCV"<<endl;
-    		if (currentSCV < impTable.NumberSCV(fromOld))
-    		{
-    			//cout<<" I am here "<<impTable.NumberSCV(fromOld)<<endl;
-    			fromOld = timet;
-    			//timet = fromOld;
-    		}
-    	}
-    	ft.setFeasibleTable(fromOld);
-    	cout<<"number of violations in iteration "<<j << " is "<< impTable.NumberSCV(ft.getFeasibleTable());
-    }
+  /*
+  CourseRoomTime crt = impTable.voteOut(0, ft.getFeasibleTable());
+  auto invitations = impTable.invites(crt, ft.getFeasibleTable());
+  auto invited = impTable.inviteMinIn(invitations, ft.getFeasibleTable());
+  cout<<endl<<endl<<"Voted out "<<crt.toString()<<" best invite is
+  "<<invited<<endl;
+  //impTable.writeTimetableToFile2();
+  auto newT = impTable.applyInvite(invited, crt, ft.getFeasibleTable());
+  cout<<endl<<endl<<" New Timetable has "<<ft.NumberHCV(newT)<<" number of hard
+  constraint violations"<<endl;
+  cout<<" New Timetable has "<<impTable.NumberSCV(newT)<<" number of hard
+  constraint violations"<<endl;
+  */
 
+  impTable.writeTimetableToFile2();
 
-    //vector<int> chromosome;
-    //auto timet = impTable.applyChomosome(chromosome, ft.getFeasibleTable());
-    //auto timet = impTable.bestNeighbour(10, ft.getFeasibleTable());
-    auto newT = timet;
-    std::stringstream ss;
-    ss << "Solutions/" << data->getName();
-    impTable.writeTimetableToFileSolutionFormat(ss.str(), timet);
+  delete data;
+  // cout<<"Maximum period is: "<<maximumPeriod;
 
-
-
-
-
-
-
-    double wall6 =   SM::get_wall_time();
-    double cpu6 = SM::get_cpu_time();
-	std::cout <<endl<< "Full Timetable construction " <<  wall6 - wall0 <<" wall seconds and "<<  cpu6 - cpu0 <<" CPU seconds"<<endl;
-
-
-	int roomStabilityViolations =   0;
-	int courseWorkingDayViolations =   0;
-	int consecutiveLectureViolations =   0;
-	int sizeViolations = 0;
-
-	cout<<endl<<endl<<"***********************Improved Timetable2*****************"<<endl;
-	cout<<endl<<endl<<" New Timetable has "<<ft.NumberHCV(newT)<<" number of hard constraint violations"<<endl;
-	cout<<" New Timetable has "<<impTable.NumberSCV(newT,&roomStabilityViolations, &courseWorkingDayViolations, &consecutiveLectureViolations, &sizeViolations)<<" number of soft constraint violations"<<endl;
-	cout<<"Room Size Violations: "<<sizeViolations<<" Room Stability: " << roomStabilityViolations << " Course Working Day Violations: " << courseWorkingDayViolations << " Consecutive Lecture Violations: " << consecutiveLectureViolations<<endl;
-
-	cout<<endl<<"**************Old Timetable**********************"<<endl;
-	cout<<" Timetable has "<<ft.NumberHCV()<<" number of hard constraint violations"<<endl;
-	cout<<" Old Timetable has "<<impTable.NumberSCV(ft.getFeasibleTable(),&roomStabilityViolations, &courseWorkingDayViolations, &consecutiveLectureViolations, &sizeViolations)<<" number of soft constraint violations"<<endl;
-	cout<<"Room Size Violations: "<<sizeViolations<<" Room Stability: " << roomStabilityViolations << " Course Working Day Violations: " << courseWorkingDayViolations << " Consecutive Lecture Violations: " << consecutiveLectureViolations<<endl;
-
-	cout<<"Threaded HCV: "<<SM::ThreadNumberHCV(newT, (maximumPeriod + 1));
- 	impTable.writeTimetableToFile("ImproveTab.csv");
-
-
-
- 	/*
-	CourseRoomTime crt = impTable.voteOut(0, ft.getFeasibleTable());
-	auto invitations = impTable.invites(crt, ft.getFeasibleTable());
-	auto invited = impTable.inviteMinIn(invitations, ft.getFeasibleTable());
-	cout<<endl<<endl<<"Voted out "<<crt.toString()<<" best invite is "<<invited<<endl;
-	//impTable.writeTimetableToFile2();
-	auto newT = impTable.applyInvite(invited, crt, ft.getFeasibleTable());
-	cout<<endl<<endl<<" New Timetable has "<<ft.NumberHCV(newT)<<" number of hard constraint violations"<<endl;
-	cout<<" New Timetable has "<<impTable.NumberSCV(newT)<<" number of hard constraint violations"<<endl;
-	*/
-
-	impTable.writeTimetableToFile2();
-
-
-	delete data;
-	//cout<<"Maximum period is: "<<maximumPeriod;
-
-	return 0;
+  return 0;
 }
